0201f3c8c24369a95a8dacba382d3bcd11b5ab1e
Jay Gmail <jrostosk@gmail.com>
2024-06-14 15:28:12 -0700

    Finished adding HEARTS

--- src/com/games/blackjack/Card.java
+++ src/com/games/blackjack/Card.java
@@ -3,5 +3,12 @@ package com.games.blackjack;
 enum Card {
     ACE_SPADES,
     TWO_SPADES,
-    THREE_SPADES
+    THREE_SPADES,
+    //
+
+    ACE_HEARTS,
+    TWO_HEARTS,
+    THREE_HEARTS,
+    FOUR_HEARTS,
+    FIVE_HEARTS
 }
