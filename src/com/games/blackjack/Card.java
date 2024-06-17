a22496652842b4508bf69f718826f96c31976e64
Jay Rostosky <jay.rostosky@tlgcohort.com>
2024-06-16 23:35:01 -0700

    Controller and main-class, welcome() and promptForPlayer()

--- src/com/games/blackjack/Card.java
+++ src/com/games/blackjack/Card.java
@@ -3,12 +3,5 @@ package com.games.blackjack;
 enum Card {
     ACE_SPADES,
     TWO_SPADES,
-    THREE_SPADES,
-    FOUR_SPADES,
-    FIVE_SPADES,
-    ACE_HEARTS,
-    TWO_HEARTS,
-    THREE_HEARTS,
-    FOUR_HEARTS,
-    FIVE_HEARTS
+    THREE_SPADES
 }
--- src/com/games/blackjack/Player.java
+++ src/com/games/blackjack/Player.java
@@ -1,4 +1,9 @@
 package com.games.blackjack;
 
 class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
 }
